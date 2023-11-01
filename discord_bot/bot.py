2cfd97fa9ee91c958549338ffa0824a70af876f6
Roman Inozemtsev <dev@koyote92.ru>
2023-11-02 04:49:50 +0500

    testing version

---
+++ discord_bot/bot.py
@@ -0,0 +1,64 @@
+import os
+
+import discord
+import responses
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+SERVER_ID = int(os.getenv('MY_SERVER_ID'))
+SYS_CHA = int(os.getenv('SYS_CHANNEL'))
+
+
+async def send_message(client, message, user_message, is_private):
+    try:
+        response = responses.handle_response(client, user_message, SERVER_ID)
+        await (message.author.send(response) if is_private
+               else await message.channel.send(response))
+    except TypeError:
+        pass
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    client = discord.Client(intents=discord.Intents.all())
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} на боевом дежурстве!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+        # message_id = int(message.id)
+        # user_presence = str(message.author.status)
+        # guild_id = int(message.guild.id)
+        # guild_name = str(message.guild.name)
+
+        print(f'{username} сказал: "{user_message}" ({channel})')
+        # print(user_presence)
+        # print(message.channel.id)
+        # print(guild_id, guild_name)
+
+        if message.channel.id == SYS_CHA:
+            print('Это важное сообщение')
+
+        if user_message.startswith('!'):
+            user_message = user_message[1:]
+            await send_message(client, message, user_message, is_private=False)
+        elif user_message.startswith('?'):
+            user_message = user_message[1:]
+            await send_message(client, message, user_message, is_private=True)
+            await message.delete()
+        else:
+            pass
+
+    client.run(BOT_TOKEN)
---
+++ discord_bot/responses.py
@@ -0,0 +1,47 @@
+import random
+
+from service import texts
+
+
+def handle_response(client, message, server_id) -> str:
+    p_message = message.lower()
+
+    if p_message in texts.greetings:
+        return 'Здорова, боец!'
+
+    elif p_message == 'кубы':
+        dices = str(random.randint(1, 6))
+        return f'На кубиках выпало {dices}, солдат.'
+
+    elif p_message == 'помощь':
+        return texts.help_text
+
+    elif p_message == 'перекличка':
+        guild = client.get_guild(server_id)
+        playing_members = dict()
+
+        for member in guild.members:
+            if member.activity:
+                playing_members[member.name] = member.activity.name
+
+        if playing_members:
+            separator = '-' * 50
+            roll_call = 'Начинаю перекличку!\n'
+            roll_call += separator
+            roll_call += '\nБойцы играют в:\n\n'
+            players_count_by_game = dict()
+            for k, v in playing_members.items():
+                roll_call += f'{k}: {v}"\n'
+                players_count_by_game[v] = players_count_by_game.get(v, 0) + 1
+            roll_call += separator
+            roll_call += '\nКоличество игроков в играх:\n\n'
+            for k, v in players_count_by_game.items():
+                roll_call += f'{k}: {v}\n'
+            roll_call += separator
+            roll_call += '\nПерекличка окончена, старшой!'
+            return roll_call
+
+        return 'Никакой активности, старшой.'
+
+    else:
+        pass
