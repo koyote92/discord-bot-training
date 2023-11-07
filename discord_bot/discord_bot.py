96917981d97237c744997d4b5c08a7a1821b6ea4
Roman Inozemtsev <dev@koyote92.ru>
2023-11-07 18:53:28 +0500

    try except

--- discord_bot/discord_bot.py
+++ discord_bot/discord_bot.py
@@ -60,8 +60,8 @@ def build_attachments(message: discord.Message) -> list[discord.File]:
 def remove_files(message: discord.Message) -> None:
     """ Удаляем загруженные медиа-файлы. """
     for attc in message.attachments:
-        print(f'Удаляем {attc.filename} ...')
         try:
+            print(f'Удаляем {attc.filename} ...')
             os.unlink(MEDIA_PATH + attc.filename)
             print(f'Файл {MEDIA_PATH + attc.filename} удалён.')
         except:
